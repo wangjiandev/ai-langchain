2fe5ec32f7540eb94ec79dc66b88eed44283ad68
Wang Jian <wangjian0504@gmail.com>
2025-05-10 10:16:13 +0800

    搜索

--- src/retriever.ts
+++ src/retriever.ts
@@ -8,13 +8,8 @@ import { OpenAIEmbeddings } from '@langchain/openai'
 import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 import { FaissStore } from '@langchain/community/vectorstores/faiss'
 import faiss from 'faiss-node'
-import { MultiQueryRetriever } from 'langchain/retrievers/multi_query'
-import { LLMChainExtractor } from 'langchain/retrievers/document_compressors/chain_extract'
-import { ContextualCompressionRetriever } from 'langchain/retrievers/contextual_compression'
 
 const main = async () => {
-    process.env.LANGCHAIN_VERBOSE = 'true'
-
     const llm = new ChatOpenAI({
         model: 'qwen3-235b-a22b',
         configuration: {
@@ -31,15 +26,23 @@ const main = async () => {
         },
     })
 
-    const directory = './db/bailuyuan'
-    const vectorstore = await FaissStore.load(directory, embeddings)
-    const compressor = LLMChainExtractor.fromLLM(llm)
-    const retriever = new ContextualCompressionRetriever({
-        baseCompressor: compressor,
-        baseRetriever: vectorstore.asRetriever(2),
+    const loader = new TextLoader('bai.txt')
+    const docs = await loader.load()
+
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 200,
+        chunkOverlap: 20,
     })
-    const result = await retriever.invoke('白嘉轩第六个妻子姓什么?')
-    console.log(result)
+
+    const splitDocs = await splitter.splitDocuments(docs)
+    const vectorStore = new FaissStore(embeddings, {})
+    const batchSize = 10
+    for (let i = 0; i < splitDocs.length; i += batchSize) {
+        const batch = splitDocs.slice(i, i + batchSize)
+        await vectorStore.addDocuments(batch)
+    }
+    const directory = './db/bailuyuan'
+    await vectorStore.save(directory)
 }
 
 main()
